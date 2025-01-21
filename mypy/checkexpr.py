44bf5a289215adfebf54ae7ecea948a167ffaba0
Brian Schubert <brianm.schubert@gmail.com>
2025-01-21 18:46:33 -0500

    Fix checking of `isinstance` calls with PEP-613 aliases

--- mypy/checkexpr.py
+++ mypy/checkexpr.py
@@ -537,7 +537,7 @@ class ExpressionChecker(ExpressionVisitor[Type]):
                     isinstance(typ, NameExpr)
                     and node
                     and isinstance(node.node, TypeAlias)
-                    and not node.node.no_args
+                    and node.node.alias_tvars
                     and not (
                         isinstance(union_target := get_proper_type(node.node.target), UnionType)
                         and union_target.uses_pep604_syntax
