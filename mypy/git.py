9f821407d28097993044d240005ad8eb7dfc0d74
Brian Schubert <brianm.schubert@gmail.com>
2025-06-28 14:20:57 -0400

    Show distance since last release branch cut in dev version number

--- mypy/git.py
+++ mypy/git.py
@@ -32,3 +32,30 @@ def is_dirty(dir: str) -> bool:
     """Check whether a git repository has uncommitted changes."""
     output = subprocess.check_output(["git", "status", "-uno", "--porcelain"], cwd=dir)
     return output.strip() != b""
+
+
+def latest_version_tag(dir: str) -> bytes:
+    """Get the name of the latest version tag."""
+    result = subprocess.run(
+        # List version tags in descending order using version-aware sorting.
+        ["git", "tag", "--sort=-version:refname", "--list", "v*"],
+        cwd=dir,
+        capture_output=True,
+        check=True,
+    )
+    return result.stdout.split(b"\n")[0]
+
+
+def dev_distance(dir: str) -> int:
+    """Return the number of commits since the last release branch was cut."""
+    version_tag = latest_version_tag(dir).decode("utf-8")
+    # Count the number of commits reachable from HEAD but not reachable from
+    # the latest version tag. If for whatever reason no suitable release tag
+    # was found, this will return 0.
+    result = subprocess.run(
+        ["git", "rev-list", "--count", f"{version_tag}..HEAD"],
+        cwd=dir,
+        capture_output=True,
+        check=True,
+    )
+    return int(result.stdout)
